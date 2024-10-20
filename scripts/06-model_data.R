e8803549426a333be0c645958b4e7086a8b41fc6
Tim Chen <timchen0326ca@gmail.com>
2024-10-20 14:19:45 -0400

    first attempt at a complete model with all the write-ups

--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -15,21 +15,50 @@ library(rstanarm)
 #### Read data ####
 analysis_data <- read_csv("data/02-analysis_data/analysis_data.csv")
 
-### Model data ####
-glm_model <- 
-  stan_glm(
-    formula = candidate_binary ~ numeric_grade + pollscore + transparency_score + sample_size + party + state + num_supporters,
-    data = analysis_data,
-    family = binomial(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 304
-  )
+harris_data <- subset(analysis_data, candidate_name == "Kamala Harris")
+
+#### Model data ####
+# Convert categorical variables to factors for Kamala Harris
+harris_data$state <- as.factor(harris_data$state)
+harris_data$methodology <- as.factor(harris_data$methodology)
+
+# Build MLR model for Kamala Harris
+mlr_harris_model <- lm(pct ~ numeric_grade + pollscore + transparency_score + sample_size + state + methodology, 
+                       data = harris_data)
 
 #### Save model ####
-saveRDS(
-  glm_model,
-  file = "models/glm_model.rds"
-)
+# Save the model to an .rds file for future use
+saveRDS(mlr_harris_model, file = "models/mlr_harris_model.rds")
+
+
+# Filter for Donald Trump data only
+trump_data <- subset(analysis_data, candidate_name == "Donald Trump")
+
+#### Model data ####
+# Convert categorical variables to factors for Donald Trump
+trump_data$state <- as.factor(trump_data$state)
+trump_data$methodology <- as.factor(trump_data$methodology)
+
+# Build MLR model for Donald Trump
+mlr_trump_model <- lm(pct ~ numeric_grade + pollscore + transparency_score + sample_size + state + methodology, 
+                      data = trump_data)
+
+#### Save model ####
+# Save the model to an .rds file for future use
+saveRDS(mlr_trump_model, file = "models/mlr_trump_model.rds")
+
+
+mlr_harris_model_refined <- lm(pct ~ numeric_grade + pollscore + sample_size + state, data = harris_data)
+
+saveRDS(mlr_harris_model_refined, file = "models/mlr_harris_model_refined.rds")
+
+
+mlr_harris_model_final <- lm(pct ~ pollscore + log(sample_size) + state, data = harris_data)
+mlr_trump_model_final <- lm(pct ~ pollscore + log(sample_size) + state, data = trump_data)
+
+saveRDS(mlr_harris_model_final, file = "models/mlr_harris_model_final.rds")
+saveRDS(mlr_trump_model_final, file = "models/mlr_trump_model_final.rds")
+
+
+
 
